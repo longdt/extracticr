975a6f25c0f271505535d8c1329344efe46407cf
longdt <longdt@localhost>
2015-03-12 15:22:27 +0700

    sort input files in test folder

--- src/testengine.cpp
+++ src/testengine.cpp
@@ -8,41 +8,45 @@
 #include <vector>
 
 #include "util/misc.h"
+#include <algorithm>
 using boost::filesystem::directory_iterator;
 using boost::filesystem::path;
 std::string chqName;
-int icrmain()
+int main()
 {
 	path p("/home/thienlong/cheque/500 Cheques/ValidChq");
 	if (!exists(p) || !is_directory(p)) {
 		return 0;
 	}
+	vector<path> files;
+	std::copy(directory_iterator(p), directory_iterator(), std::back_inserter(files));
+	std::sort(files.begin(), files.end());
 	icr::ICREngine::loadModels("models");
 	icr::ICREngine engine;
 	int skip = 1 - 1; //*73 *169 313 330 398 *42 *78
 	int counter = 0;
 	int end = 1000;
-	for (directory_iterator iter(p), iterend; iter != iterend; ++iter) {
+	for (auto iter = files.begin(), iterend = files.end(); iter != iterend; ++iter) {
 		++counter;
 		if (counter <= skip) {
 			continue;
 		} else if (counter > end) {
 			break;
 		}
-		cv::Mat cheque = cv::imread(iter->path().string(), 0);
+		cv::Mat cheque = cv::imread(iter->string(), 0);
 //		cv::Mat cheque = cv::imread("/home/thienlong/cheque/500 Cheques/ValidChq/chq_00461_00.jpeg", 0);
 		if (cheque.empty())
 			return -1;
-		chqName = iter->path().filename().string();
+		chqName = iter->filename().string();
 		std::size_t pos = chqName.find(".");
 
 		chqName = chqName.substr(0, pos);
-		std::cout << counter << "\t" << iter->path().filename().string() << "\t";
+		std::cout << counter << "\t" << iter->filename().string() << "\t";
 		std::cout.flush();
 		std::string amountStr = removeDelimiter(engine.recognite(cheque));
 //		long double amount = std::stold(amountStr);
 		std::cout << amountStr << std::endl;
-//		cv::waitKey(100);
+		cv::waitKey(0);
 //		cv::destroyAllWindows();
 	}
     return 0;
